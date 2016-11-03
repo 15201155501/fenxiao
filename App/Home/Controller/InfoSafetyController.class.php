958307bd6e1b02143c28fee8f493613ee9bf753b
spc <15201155501@163.com>
2016-11-03 16:08:03 +0800

    会员激活

--- App/Home/Controller/InfoSafetyController.class.php
+++ App/Home/Controller/InfoSafetyController.class.php
@@ -1,8 +1,29 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends CommonController {
-    public function index(){
-    	$this->display();
+class InfoSafetyController extends CommonController {
+    /**
+     * 修改个人资料
+     */
+    public function info_upd(){
+        //判断是否输入过三级密码
+        if(empty(I('post.password3'))){
+            $this->display('passthree');
+        }else{
+            //获取提交的表单数据
+            $pwd = I('post.password3');
+            //获取登录的用户id
+            $map['HyNumber'] = session('hynumber');
+            $res = M('hyclub')->where($map)->find();
+
+            if($res && $res['hypassword3']==$pwd){
+                $this->assign('userinfo',$res);
+
+                //渲染模板
+                $this->display();
+            }else{
+                $this->error('密码输入错误');
+            }
+        }
     }
 }
--- App/Home/Controller/ManagementTeamController.class.php
+++ App/Home/Controller/ManagementTeamController.class.php
@@ -46,6 +46,32 @@ class ManagementTeamController extends CommonController {
         $lefts = A('Home/Public');
         $left=$lefts->left();
         $this->assign('userinfo',$left);
+        $table = "hyclub";
+        $table2 = "ddgl";
+
+       $count      = $Model->where("BelongWuliuNumber='$hynumber'")->count();// 查询满足要求的总记录数
+        print_r($count);
+        $Page       = new \Think\Page($count,4);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出
+// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $list=$Model->table('hyclub h','ddgl d')
+            ->field('h.ID,h.HyJoinInvest,h.HyNumber,h.hyname,h.IsApproved,h.ApprovedTime,h.HyParentNumber,h.hytjnumber,h.RegisterTime,d.wlmid,d.dmeonty')
+            ->join('ddgl d ON d.wlmid = h.ID')
+            ->order('h.RegisterTime desc' )
+            ->where("h.BelongWuliuNumber='$hynumber'")
+            ->limit($Page->firstRow.",".$Page->listRows)
+            ->select();
+       // $Demo->join('RIGHT JOIN think_work ON think_artist.id = think_work.artist_id' );
+        echo "<pre>";
+        print_r($list);
+        echo "</pre>";
+
+
+
+
+        $this->assign('list',$list);// 赋值数据集
+        $this->assign('page',$show);// 赋值分页输出*/
+
 
 
 
